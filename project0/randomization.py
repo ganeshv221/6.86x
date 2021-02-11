0b3c88a882bc9fd06320a8a8605e156e51a5a076
ganeshv221 <ganeshv221@gmail.com>
2021-02-11 11:35:02 +0530

    first commit

---
+++ project0/randomization.py
@@ -0,0 +1,11 @@
+def randomization(n):
+    """
+    Arg:
+      n - an integer
+    Returns:
+      A - a randomly-generated nx1 Numpy array.
+    """
+    #Your code here
+    A=np.random.random([n,1])
+    return A
+    raise NotImplementedError
